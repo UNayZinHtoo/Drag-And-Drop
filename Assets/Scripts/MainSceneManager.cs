21cd586bda30d53cdeeb6dfabb1c4074adb1e897
Nay Zin Htoo <nayzinhtoo.360ed@gmail.com>
2020-10-20 10:16:29 +0630

    NZH: update scene manager

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainSceneManager : MonoBehaviour
 {
+    public String sceneName;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,17 @@ public class MainSceneManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-            Application.Quit();
+        {
+            if (sceneName=="MainScene")
+            {
+                Application.Quit();
+            }
+            else
+            {
+                LoadScene("MainScene");
+            }
+        }
+            
     }
     public void LoadScene(string sceneName)
     {
