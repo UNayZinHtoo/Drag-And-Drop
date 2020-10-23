13487fb2d2f46445698c405e3a2b938cd2f54326
Nay Zin Htoo <nayzinhtoo.360ed@gmail.com>
2020-10-23 09:58:49 +0630

    NZH: add Eg Scene

---
+++ Assets/Scripts/ItemClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemClick : MonoBehaviour, IPointerClickHandler
+{
+    private GameObject parentGameObject;
+    public bool clicked;
+    
+    void Start()
+    {
+        parentGameObject = GameObject.FindGameObjectWithTag("AnsPanel");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clicked) {
+            transform.SetParent(parentGameObject.transform);
+        }
+    }
+}
